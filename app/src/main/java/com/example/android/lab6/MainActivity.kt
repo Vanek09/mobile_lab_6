184fa5c0ec7f2574852c81312f49749ffb2a4c9a
Vanek Korobka <korobka.ivan09@gmail.com>
2022-10-17 20:51:50 +0300

    Task 1 completed

--- app/src/main/java/com/example/android/lab6/MainActivity.kt
+++ app/src/main/java/com/example/android/lab6/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.android.lab6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.android.lab6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.showResultButton.setOnClickListener { onClick() }
+    }
+
+    private fun onClick() {
+        if (isEditTextsEmpty()) {
+            Toast.makeText(this, "Enter values in EditTexts!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        binding.apply {
+            val a = Integer.parseInt(aEditText.text.toString())
+            val b = Integer.parseInt(bEditText.text.toString())
+            val c = Integer.parseInt(cEditText.text.toString())
+            val d = Integer.parseInt(dEditText.text.toString())
+            val x = Integer.parseInt(xEditText.text.toString())
+
+            showFirstResult(a, b, c, d)
+            showSecondResult(x)
+            showThirdResult(a, b, c, x)
+        }
+    }
+
+    private fun isEditTextsEmpty(): Boolean {
+        binding.apply {
+            return aEditText.text.toString().isEmpty() || bEditText.text.toString().isEmpty() ||
+                    cEditText.text.toString().isEmpty() || dEditText.text.toString().isEmpty() ||
+                    xEditText.text.toString().isEmpty()
+        }
+    }
+
+    private fun showFirstResult(vararg numbers: Int) {
+        numbers.sort()
+        binding.firstResult.text = "y = ${numbers.last()}"
+    }
+
+    private fun  showSecondResult(x: Int) {
+        binding.secondResult.text = "y = ${x*x*x*x}"
+    }
+
+    private fun showThirdResult(a: Int, b: Int, c: Int, x: Int) {
+        binding.thirdResult.text = "y = ${a * x * x + b * x + c}"
     }
 }
